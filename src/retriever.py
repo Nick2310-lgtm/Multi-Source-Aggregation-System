3fc613b04662c44cdd9335fcba21c4af6c80a988
Nicholas Coutinho <nicholas.s@somaiya.edu>
2026-04-21 12:56:44 +0530

    Update retriever.py

--- src/retriever.py
+++ src/retriever.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def generate_embeddings(docs):
-    return model.encode(docs)
+    return model.encode(docs, normalize_embeddings=True)
 
 def embed_query(query):
-    return model.encode([query])
+    return model.encode([query], normalize_embeddings=True)
